40e6d7414cf288256d9f6becbfb7dd04eb93f482
Alexander0044 <AlexclanRoyal@gmail.com>
2025-10-08 03:06:41 +0100

    Add files via upload

---
+++ readme.mk
@@ -0,0 +1,28 @@
+# Proyecto de Equipos de Baloncesto XML
+
+## Descripción del Proyecto
+Este proyecto contiene un archivo XML y un DTD que definen una pequeña base de datos de equipos de baloncesto.  
+El archivo XML contiene varios equipos, cada uno con un entrenador y jugadores. Cada jugador tiene un ID, nombre, posición, número y edad.  
+El DTD asegura que el archivo XML esté bien formado y sea válido.
+
+## Cómo Empezar
+Para usar o validar este proyecto:
+
+1. Descarga los archivos `basketball_team.xml` y `basketball_team.dtd`.
+2. Coloca ambos archivos en la misma carpeta.
+3. Abre `basketball_team.xml` con un editor de texto o visor de XML.
+
+## Agradecimientos
+
+Este proyecto se realizó con la ayuda de las siguientes guias:
+
+- [Tutorial de XML - W3Schools](https://www.w3schools.com/xml/)
+- [Guía de DTD - TutorialsPoint](https://www.tutorialspoint.com/xml/xml_dtds.htm)
+
+## Autor
+
+- Alexander Marrero Bermudez
+
+## Plantilla Usada
+
+Este README se basa en la **Plantilla Básica de GitHub** de [readme.so](https://readme.so).
